845d07fabccbe7911fbed4dcfb67a9e9e59238ce
NetryCPPP <pvasma404@gmail.com>
2026-02-28 00:21:25 +0530

    lanya.js

--- lanya.js
+++ lanya.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Database Connected'));
 const express = require('express');
 const app = express();
 
